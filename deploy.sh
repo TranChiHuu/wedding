108dfd3f35e9a895668e9c1aa7ec8643f0a70d91
HuuTC <32271313+TranChiHuu@users.noreply.github.com>
2024-09-16 17:09:05 +0700

    update code deploy

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ echo "Build complete."
 
 # Copy the build files to the S3 bucket
 echo "Copying build files to S3..."
-aws s3 sync dist s3://trinhhuu/
+aws s3 sync dist s3://trinhhuu/ --profile michael
 echo "Files copied to S3."
 
 # Create a CloudFront invalidation
 echo "Creating CloudFront invalidation..."
-aws cloudfront create-invalidation --distribution-id E1MC6EN7EN2PXN --paths '/*'
+aws cloudfront create-invalidation --distribution-id E1MC6EN7EN2PXN --paths '/*' --profile michael
 echo "CloudFront invalidation complete."
 
 echo "Deployment script finished successfully."
